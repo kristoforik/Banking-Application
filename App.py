2d16267879152d6a6965752b2bcbe6ab7c527df0
Andrii Kudenko <kudenko4614@gmail.com>
2022-11-14 02:28:44 -0500

    addind classes

---
+++ App.py
@@ -0,0 +1,37 @@
+
+class Program:
+    def run(self):
+        pass
+    def showMainMenu(self):
+        pass
+    def showAccountMenu(self):
+        pass
+
+class Bank:
+    def __init__(self):
+        pass
+    def openAccount(self):
+        pass
+    def searchAccount(self):
+        pass
+
+class Account:
+    def __init__(self, n, a, r, b):
+        self.accountNumber = n
+        self.accountHoldName = a
+        self.rateOfInterest = r
+        self.currentBalance = b
+    def getAccountNumber(self):
+        pass
+    def getAccountHoldName(self):
+        pass
+    def getRateOfInterest(self):
+        pass
+    def getCurrentBalance(self):
+        pass
+    def deposit(self):
+        pass
+    def withdraw(self):
+        pass
+
+
