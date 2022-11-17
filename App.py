07838fbcf119fad66e8798aa0bf0e12bfdf7fd02
Andrii Kudenko <kudenko4614@gmail.com>
2022-11-17 17:48:31 -0500

    upgrading account menu and adding checks

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-
+import time
 '''account_numbers = [100, 200, 300, 400, 500]
 accounts = {
     100 : [100, 'John', 0, 2400.25]
@@ -15,6 +15,35 @@ def account_num_checking(accounts, number):
             if accounts[n].accountNumber == number:
                 return n
 
+def check_name(name):
+    chname = str(name)
+    while chname.isalpha() == False:
+        chname = input("Enter a valid name: ")
+    return chname
+
+def check_number(number):
+    tnumber = str(number)
+    cnumber = tnumber
+    while cnumber.isnumeric() == False:
+        cnumber = (input("Enter a valid number: "))
+    return cnumber
+
+def check_amount(amount):
+    camount = amount
+    while True:
+        try:
+            float(camount)
+            return camount
+        except ValueError:
+            camount = input("Enter a valid product price: ")
+
+def creating_account():
+    number = input("Enter the number for your account: ")
+    number = check_number(number)
+    name = input("Enter your name: ")
+    name = check_name(name)
+    return Account(number, name, 0, 0)
+
 class Program:
     def run(self):
         self.showMainMenu()
@@ -22,7 +51,8 @@ class Program:
         print("[1] Open an account")
         print("[2] Select an account")
         print("[3] Exit the application")
-        choice = int(input("Enter the number: "))
+        choice = (input("Enter the number: "))
+        choice = int(check_number(choice))
         if choice == 1:
             newAcc = Bank()
             newAcc.openAccount()
@@ -44,31 +74,56 @@ class Program:
         attempt = int(input("Enter the number: "))
         if attempt == 1:
             print(account.getCurrentBalance())
+            time.sleep(0.5)
+            print("[1] Exit to account menu")
+            print("[2] Exit to main menu")
+            print("[3] Exit the application")
+            choice = input("Enter the number: ")
+            if choice == 1:
+                self.showAccountMenu(account)
+            elif choice == 2:
+                self.showMainMenu(account)
+            elif choice == 3:
+                quit()
         elif attempt == 2:
-            pass
+            amount = input("Enter the amount to deposit: ")
+            amount = float(check_amount(amount))
+            account.deposit(amount)
+            print(account.getCurrentBalance())
+            time.sleep(0.5)
+            print("Money successfully deposited")
+            time.sleep(0.5)
+            self.showAccountMenu(account)
         elif attempt == 3:
             pass
         elif attempt == 4:
             pass
         else:
             print("Wrong")
-
+        return account
 
 class Bank:
     def __init__(self):
+        user1 = Account(100, 'John', 0, 2350.25)
+        user2 = Account(200, 'Mike', 0, 1934.40)
+        user3 = Account(300, 'Richard', 0, 65983.21)
+        user4 = Account(400, 'Andrew', 0, 15.90)
+        user5 = Account(500, 'Jason', 0, 159693.01)
         self.bankName = 'CIBC'
-    def openAccount(self):
-        pass
+        self.account_list = [user1, user2, user3, user4, user5]
+    def openAccount(self, account):
+        tmp = account
+        self.account_list.append(tmp)
+        return self.account_list
     def searchAccount(self):
         attempt = int(input("Enter the account number: "))
-        result = account_num_checking(accounts, attempt)
+        result = account_num_checking(self.account_list, attempt)
         if result == None:
             print("Wrong number")
         else:
-            user_account = accounts[result]
+            user_account = self.account_list[result]
             account_menu = Program()
             account_menu.showAccountMenu(user_account)
-
 
 class Account:
     def __init__(self, number, name, rate, balance):
@@ -84,35 +139,41 @@ class Account:
         return self.rateOfInterest
     def getCurrentBalance(self):
         return self.currentBalance
-    def deposit(self):
-        pass
+    def deposit(self, amount):
+        self.currentBalance = self.currentBalance + amount
+        return self.currentBalance
     def withdraw(self):
         pass
 
-class SavingsAccount:
+class SavingsAccount(Account):
     def __init__(self):
-            self.minimumBalance = 0
+        super().__init__()
+        self.minimumBalance = 0
     def withdraw(self):
         pass
     
-class SavingsAccount:
+class SavingsAccount(Account):
     def __init__(self):
-            self.overdraftAllowed = 0
+        super().__init__()
+        self.overdraftAllowed = 0
     def withdraw(self):
         pass
 
 
+
+
 '''a = accounts[0]
-print(a[0])'''
+print(a[0])
 
 user1 = Account(100, 'John', 0, 2350.25)
 user2 = Account(200, 'Mike', 0, 1934.40)
 user3 = Account(300, 'Richard', 0, 65983.21)
 user4 = Account(400, 'Andrew', 0, 15.90)
 user5 = Account(500, 'Jason', 0, 159693.01)
-accounts = [user1, user2, user3, user4, user5]
+accounts = [user1, user2, user3, user4, user5]'''
 
-
+#bank = Bank()
+#print(bank.account_list[1].getAccountNumber())
 
 
 user = Program()
