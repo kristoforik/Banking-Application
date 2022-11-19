2f9749a27557f3346a8793410fa8e7acab672729
Andrii Kudenko <kudenko4614@gmail.com>
2022-11-18 21:51:43 -0500

    added chequing and saving accounts

--- App.py
+++ App.py
@@ -39,15 +39,15 @@ def check_amount(amount):
 
 def creating_account():
     number = input("Enter the number for your account: ")
-    number = check_number(number)
+    number = int(check_number(number))
     name = input("Enter your name: ")
     name = check_name(name)
-    return Account(number, name, 0, 0)
+    return Account(number, name, 0, 0, 0, 0, 0)
 
 class Program:
-    def run(self):
-        self.showMainMenu()
-    def showMainMenu(self):
+    def run(self, account_list):
+        self.showMainMenu(account_list)
+    def showMainMenu(self, account_list):
         print("[1] Open an account")
         print("[2] Select an account")
         print("[3] Exit the application")
@@ -55,17 +55,25 @@ class Program:
         choice = int(check_number(choice))
         if choice == 1:
             newAcc = Bank()
-            newAcc.openAccount()
+            newAcc.openAccount(account_list)
         elif choice == 2:
             selAcc = Bank()
-            selAcc.searchAccount()
+            selAcc.searchAccount(account_list)
         elif choice == 3:
             quit()
         else:
             print("Wrong")
-    def showAccountMenu(self, account):
+    def showAccountMenu(self, account, account_list):
         print("Holder's name: ", account.getAccountHoldName())
         print("Holder's account number: ", account.getAccountNumber())
+        print("Which account do you want to access?")
+        print("[1] Checking account [2] Savings account")
+        choice = int(input("Enter the number: "))
+        if choice == 1:
+            self.showCheqAccountMenu(account, account_list)
+        elif choice == 2:
+            self.showSavAccountMenu(account, account_list)
+    def showCheqAccountMenu(self, account, account_list):
         print("What do you want to do?")
         print("[1] Check balance") 
         print("[2] Deposit money") 
@@ -74,105 +82,139 @@ class Program:
         print("[5] Exit the application")
         attempt = int(input("Enter the number: "))
         if attempt == 1:
-            print(account.getCurrentBalance())
-            time.sleep(0.5)
-            print("[1] Exit to account menu")
-            print("[2] Exit to main menu")
-            print("[3] Exit the application")
-            choice = int(input("Enter the number: "))
-            if choice == 1:
-                self.showAccountMenu(account)
-            elif choice == 2:
-                self.showMainMenu()
-            elif choice == 3:
-                quit()
+            time.sleep(0.2)
+            print(account.cheqaccount.getCurrentBalance())
+            time.sleep(0.2)
+            self.showCheqAccountMenu(account, account_list)
         elif attempt == 2:
+            time.sleep(0.2)
             amount = input("Enter the amount to deposit: ")
             amount = float(check_amount(amount))
-            account.deposit(amount)
-            print(account.getCurrentBalance())
+            account.cheqaccount.deposit(amount)
+            print(account.cheqaccount.getCurrentBalance())
             time.sleep(0.5)
             print("Money successfully deposited")
             time.sleep(0.5)
-            self.showAccountMenu(account)
+            self.showCheqAccountMenu(account, account_list)
         elif attempt == 3:
             print("You withdrawed")
         elif attempt == 4:
             time.sleep(0.5)
-            self.showMainMenu(account)
+            self.showMainMenu(account_list)
+        elif attempt == 5:
+            time.sleep(0.5)
+            print("You left the application")
+            quit()
+        else:
+            print("Wrong")    
+    def showSavAccountMenu(self, account, account_list):
+        print("What do you want to do?")
+        print("[1] Check balance") 
+        print("[2] Deposit money") 
+        print("[3] Withdraw money") 
+        print("[4] Exit to main menu")
+        print("[5] Exit the application")
+        attempt = int(input("Enter the number: "))
+        if attempt == 1:
+            time.sleep(0.2)
+            print(account.savaccount.getCurrentBalance())
+            time.sleep(0.2)
+            self.showSavAccountMenu(account, account_list)
+        elif attempt == 2:
+            time.sleep(0.2)
+            amount = input("Enter the amount to deposit: ")
+            amount = float(check_amount(amount))
+            account.savaccount.deposit(amount)
+            print(account.savaccount.getCurrentBalance())
+            time.sleep(0.5)
+            print("Money successfully deposited")
+            time.sleep(0.5)
+            self.showSavAccountMenu(account, account_list)
+        elif attempt == 3:
+            print("You withdrawed")
+        elif attempt == 4:
+            time.sleep(0.5)
+            self.showMainMenu(account_list)
         elif attempt == 5:
             time.sleep(0.5)
             print("You left the application")
             quit()
         else:
             print("Wrong")
-        return account
 
 class Bank:
     def __init__(self):
-        user1 = Account(100, 'John', 0, 2350.25)
-        user2 = Account(200, 'Mike', 0, 1934.40)
-        user3 = Account(300, 'Richard', 0, 65983.21)
-        user4 = Account(400, 'Andrew', 0, 15.90)
-        user5 = Account(500, 'Jason', 0, 159693.01)
         self.bankName = 'CIBC'
-        self.account_list = [user1, user2, user3, user4, user5]
-    def openAccount(self):
+    def openAccount(self, account_list):
+        program = Program()
         tmp = creating_account()
-        self.account_list.append(tmp)
-        print(self.account_list[-1].accountHoldName)
-        return self.account_list
-    def searchAccount(self):
+        account_list.append(tmp)
+        #print(account_list)
+        program.showMainMenu(account_list)
+    def searchAccount(self, account_list):
         attempt = int(input("Enter the account number: "))
-        result = account_num_checking(self.account_list, attempt)
+        result = account_num_checking(account_list, attempt)
         if result == None:
             print("Wrong number")
         else:
-            user_account = self.account_list[result]
+            user_account = account_list[result]
             account_menu = Program()
-            account_menu.showAccountMenu(user_account)
+            account_menu.showAccountMenu(user_account, account_list)
 
 class Account:
-    def __init__(self, number, name, rate, balance):
+    def __init__(self, number, name, rate, sbalance, minbalance, cbalance, overdraft):
         self.accountNumber = number
         self.accountHoldName = name
         self.rateOfInterest = rate
-        self.currentBalance = balance
+        self.savaccount = SavingsAccount(sbalance, minbalance)
+        self.cheqaccount = ChequingAccount(cbalance, overdraft)
     def getAccountNumber(self):
         return self.accountNumber
     def getAccountHoldName(self):
         return self.accountHoldName
     def getRateOfInterest(self):
         return self.rateOfInterest
-    def getCurrentBalance(self):
+    '''def getCurrentSavingsBalance(self):
+        return self.currentBalance
+    def getCurrentChequingBalance(self):
         return self.currentBalance
     def deposit(self, amount):
         self.currentBalance = self.currentBalance + amount
-        return self.currentBalance
+        return self.currentBalance'''
     def withdraw(self):
         pass
 
-class SavingsAccount(Account):
-    def __init__(self, number, name, rate, balance):
-        super().__init__(number, name, rate, balance)
-        self.minimumBalance = 0
+class SavingsAccount():
+    def __init__(self, sbalance, minbalance):
+        self.balance = sbalance
+        self.minimumBalance = minbalance
     def withdraw(self):
         pass
+    def getCurrentBalance(self):
+        return self.balance
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self.balance
     
-class SavingsAccount(Account):
-    def __init__(self, number, name, rate, balance):
-        super().__init__(number, name, rate, balance)
-        self.overdraftAllowed = 0
+class ChequingAccount():
+    def __init__(self, cbalance, overdraft):
+        self.balance = cbalance
+        self.overdraftAllowed = overdraft
     def withdraw(self):
         pass
+    def getCurrentBalance(self):
+        return self.balance
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self.balance
 
-class CreditScore(Account):
-    def __init__(self, number, name, rate, balance):
-        super().__init__(number, name, rate, balance)
-        self.credit_score = 0
-
-
-
+# sbalance, minbalance, cbalance, overdraft
+user1 = Account(100, 'John', 0, 4000, 1000, 2350.25, 2000)
+user2 = Account(200, 'Mike', 0, 4000, 1000, 1934.40, 2000)
+user3 = Account(300, 'Richard', 0, 4000, 1000, 65983.21, 2000)
+user4 = Account(400, 'Andrew', 0, 4000, 1000, 15.90, 2000)
+user5 = Account(500, 'Jason', 0, 4000, 1000, 159693.01, 2000)
+accountList = [user1, user2, user3, user4, user5]
 
 
 '''a = accounts[0]
@@ -190,4 +232,4 @@ accounts = [user1, user2, user3, user4, user5]'''
 
 
 user = Program()
-user.run()
+user.run(accountList)
