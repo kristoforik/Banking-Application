0b2997d10a16c0ec794beb906cf130daab1509ca
Andrii Kudenko <kudenko4614@gmail.com>
2022-11-23 01:23:44 -0500

    made more checking functions and cosmetic changes

--- App.py
+++ App.py
@@ -1,15 +1,11 @@
-import time
-'''account_numbers = [100, 200, 300, 400, 500]
-accounts = {
-    100 : [100, 'John', 0, 2400.25]
-}
-nu = 100
-def account_check(accounts_numbers, number):  
-    for n in range(len(accounts_numbers)):
-        if accounts_numbers[n] == number:
-            return n
-tmp = account_check(account_numbers, nu)
-acc = account_numbers[tmp] '''
+import time, sys
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
 def account_num_checking(accounts, number):
     for n in range(len(accounts)):
             if accounts[n].accountNumber == number:
@@ -20,6 +16,14 @@ def check_name(name):
     while chname.isalpha() == False:
         chname = input("Enter a valid name: ")
     return chname
+
+def check_inlist(alist, number):
+    num = int(check_number(number))
+    for i in alist:
+        if str(num) == str(i.accountNumber):
+            num = input("The number is busy: ")
+            num = check_inlist(alist, num)
+    return num
 
 def check_onetwothree(number):
     cnumber = str(number)
@@ -55,17 +59,22 @@ def check_amount(amount):
         except ValueError:
             camount = input("Enter a valid amount: ")
 
-def creating_account():
+def creating_account(alist):
+    print_slow("\u001b[36m---------Creating Account---------\u001b[0m\n")
     number = input("Enter the number for your account: ")
     number = int(check_number(number))
+    number = check_inlist(alist, number)
     name = input("Enter your name: ")
     name = check_name(name)
     return Account(number, name, 0, 0, 0, 0, 0)
 
 class Program:
     def run(self, account_list):
+        print_slow("\u001b[36m---------\u001b[33mBank Application\u001b[36m---------\u001b[0m\n")
+        time.sleep(1)
         self.showMainMenu(account_list)
     def showMainMenu(self, account_list):
+        print_slow("\u001b[36m-------------Main Menu-------------\u001b[0m\n")
         print("[1] Open an account")
         print("[2] Select an account")
         print("[3] Exit the application")
@@ -81,11 +90,12 @@ class Program:
             selAcc.searchAccount(account_list)
         elif choice == 3:
             time.sleep(0.5)
-            print("You left the application")
+            print("\u001b[31mYou left the application\u001b[0m")
             quit()
         else:
             print("Wrong")
     def showAccountMenu(self, account, account_list):
+        print_slow("\u001b[36m------------\u001b[33mAccount Menu\u001b[36m------------\u001b[0m\n")
         print("Holder's name: ", account.getAccountHoldName())
         print("Holder's account number: ", account.getAccountNumber())
         print("Which account do you want to access?")
@@ -97,6 +107,7 @@ class Program:
         elif choice == 2:
             self.showSavAccountMenu(account, account_list)
     def showCheqAccountMenu(self, account, account_list):
+        print_slow("\u001b[36m-------\u001b[33mChequing Account Menu\u001b[36m-------\u001b[0m\n")
         print("What do you want to do?")
         print("[1] Check balance") 
         print("[2] Deposit money") 
@@ -118,7 +129,7 @@ class Program:
             account.cheqaccount.deposit(amount)
             print(account.cheqaccount.getCurrentBalance())
             time.sleep(0.5)
-            print("Money successfully deposited")
+            print("\u001b[32mMoney successfully deposited\u001b[0m")
             time.sleep(0.5)
             self.showCheqAccountMenu(account, account_list)
         elif attempt == 3:
@@ -135,11 +146,12 @@ class Program:
             self.showMainMenu(account_list)
         elif attempt == 6:
             time.sleep(0.5)
-            print("You left the application")
+            print("\u001b[31mYou left the application\u001b[0m")
             quit()
         else:
             print("Wrong")  
     def showSavAccountMenu(self, account, account_list):
+        print_slow("\u001b[36m--------\u001b[33mSavings Account Menu\u001b[36m--------\u001b[0m\n")
         print("What do you want to do?")
         print("[1] Check balance") 
         print("[2] Deposit money") 
@@ -161,7 +173,7 @@ class Program:
             account.savaccount.deposit(amount)
             print(account.savaccount.getCurrentBalance())
             time.sleep(0.5)
-            print("Money successfully deposited")
+            print("\u001b[32mMoney successfully deposited\u001b[0m")
             time.sleep(0.5)
             self.showSavAccountMenu(account, account_list)
         elif attempt == 3:
@@ -178,7 +190,7 @@ class Program:
             self.showMainMenu(account_list)
         elif attempt == 6:
             time.sleep(0.5)
-            print("You left the application")
+            print("\u001b[31mYou left the application\u001b[0m")
             quit()
         else:
             print("Wrong") 
@@ -188,21 +200,25 @@ class Bank:
         self.bankName = 'CIBC'
     def openAccount(self, account_list):
         program = Program()
-        tmp = creating_account()
+        tmp = creating_account(account_list)
         account_list.append(tmp)
         #print(account_list)
+        time.sleep(0.5)
         program.showMainMenu(account_list)
     def searchAccount(self, account_list):
         menu = Program()
-        attempt = int(input("Enter the account number: "))
+        print_slow("\u001b[36m---------Seleceting Account---------\u001b[0m\n")
+        attempt = (input("Enter the account number: "))
+        attempt = int(check_number(attempt))
         result = account_num_checking(account_list, attempt)
         if result == None:
             time.sleep(0.5)
-            print("There is no account with such number")
+            print("\u001b[31mThere is no account with such number\u001b[0m")
             time.sleep(0.5)
             menu.showMainMenu(account_list)
         else:
             user_account = account_list[result]
+            time.sleep(0.5)
             menu.showAccountMenu(user_account, account_list)
 
 class Account:
@@ -218,13 +234,6 @@ class Account:
         return self.accountHoldName
     def getRateOfInterest(self):
         return self.rateOfInterest
-    '''def getCurrentSavingsBalance(self):
-        return self.currentBalance
-    def getCurrentChequingBalance(self):
-        return self.currentBalance
-    def deposit(self, amount):
-        self.currentBalance = self.currentBalance + amount
-        return self.currentBalance'''
     def withdraw(self):
         pass
 
@@ -236,11 +245,15 @@ class SavingsAccount():
         return self.balance
     def withdraw(self, amount):
         if self.balance - amount <= self.minimumBalance:
-            print("Unfortunatelly, you cannot withdraw this amount because of the required minimum balance")
+            time.sleep(0.5)
+            print("\u001b[31mUnfortunatelly, you cannot withdraw this amount because of the required minimum balance\u001b[0m")
+            time.sleep(0.5)
             return self.balance
         else:
             self.balance -= amount
-            print("Money successfully withdrawn")
+            time.sleep(0.5)
+            print("\u001b[32mMoney successfully withdrawn\u001b[0m")
+            time.sleep(0.5)
             return self.balance
     def deposit(self, amount):
         self.balance = self.balance + amount
@@ -255,10 +268,14 @@ class ChequingAccount():
     def withdraw(self, amount):
         if self.balance - amount >= -self.overdraftAllowed:
             self.balance -= amount
-            print("Money successfully withdrawn")
+            time.sleep(0.5)
+            print("\u001b[32mMoney successfully withdrawn\u001b[0m")
+            time.sleep(0.5)
             return self.balance
         else:
-            print("Unfortunatelly, you cannot withdraw this amount as you exceed your overdraft limit")
+            time.sleep(0.5)
+            print("\u001b[31mUnfortunatelly, you cannot withdraw this amount as you exceed your overdraft limit\u001b[0m")
+            time.sleep(0.5)
             return self.balance
     def deposit(self, amount):
         self.balance = self.balance + amount
@@ -271,21 +288,6 @@ user3 = Account(300, 'Richard', 0, 4000, 1000, 65983.21, 2000)
 user4 = Account(400, 'Andrew', 0, 4000, 1000, 15.90, 2000)
 user5 = Account(500, 'Jason', 0, 4000, 1000, 159693.01, 2000)
 accountList = [user1, user2, user3, user4, user5]
-
-
-'''a = accounts[0]
-print(a[0])
-
-user1 = Account(100, 'John', 0, 2350.25)
-user2 = Account(200, 'Mike', 0, 1934.40)
-user3 = Account(300, 'Richard', 0, 65983.21)
-user4 = Account(400, 'Andrew', 0, 15.90)
-user5 = Account(500, 'Jason', 0, 159693.01)
-accounts = [user1, user2, user3, user4, user5]'''
-
-#bank = Bank()
-#print(bank.account_list[1].getAccountNumber())
-
 
 user = Program()
 user.run(accountList)
